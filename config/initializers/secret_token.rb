ebd2c562e08b8e0ee2bd9ba01a5cf44d6e7f8e6c
blaslaunchlabs <blas_delossantos@launchlabs.ph>
2016-01-29 17:13:15 +0800

    first commit toy app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = '2cfd0ca809acc9703acd430eb4da85f3df9f7b47155a8eca94d62ff7e300ee9af61197ad966c3a38961aedc46b61dcc3dd4e9c92e1ff8a73bbb3ac8874894b19'
